c3eec907d95e5a34455849b01e2b1bf321960b3c
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-11-28 17:31:37 -0700

    Added pages and their code & navbar and footer

--- camionesdeculiacan/src/app/app-routing.module.ts
+++ camionesdeculiacan/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HorariosComponent } from './pages/horarios/horarios.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'horarios', component:HorariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
