80e4e5afd65c6a52a577ce8b72a1d3138bb27b2f
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-12-07 22:05:03 -0700

    Finished schedule and alerts page

--- camionesdeculiacan/src/app/pages/horarios/horarios.component.ts
+++ camionesdeculiacan/src/app/pages/horarios/horarios.component.ts
@@ -9,9 +9,15 @@ export class HorariosComponent {
   public show:boolean = false;
   public show1:boolean = false;
   public show2:boolean = false;
+  public show3:boolean = false;
+  public show4:boolean = false;
+  public show5:boolean = false;
   public mostrar:any = 'Show';
   public mostrar1:any = 'Show';
   public mostrar2:any = 'Show';
+  public mostrar3:any = 'Show';
+  public mostrar4:any = 'Show';
+  public mostrar5:any = 'Show';
 
   toggle() {
 
@@ -29,6 +35,24 @@ export class HorariosComponent {
 
     this.show2 = !this.show2;
     this.mostrar2 = !this.mostrar2;
+  }
+
+  toggle3() {
+
+    this.show3 = !this.show3;
+    this.mostrar3 = !this.mostrar3;
+  }
+
+  toggle4() {
+
+    this.show4 = !this.show4;
+    this.mostrar4 = !this.mostrar4;
+  }
+
+  toggle5() {
+
+    this.show5 = !this.show5;
+    this.mostrar5 = !this.mostrar5;
   }
   
   
--- camionesdeculiacan/src/app/pages/menu/menu.component.ts
+++ camionesdeculiacan/src/app/pages/menu/menu.component.ts
@@ -22,18 +22,19 @@ export class MenuComponent {
 		switch(this.ruta)
 		{
 			case "1":
-				this.mapa = "https://www.google.com/maps/d/embed?mid=1PISuXdAr6nOqj05boVbYuMklQTI&hl=en_US&ehbc=2E312F";
-
-				
-				
+				this.mapa = "https://www.google.com/maps/d/embed?mid=1PISuXdAr6nOqj05boVbYuMklQTI&hl=en_US&ehbc=2E312F";				
 			break;
 
 			case "2":
-				
+				this.mapa = "https://www.google.com/maps/d/embed?mid=1fF_NoA1aag79j6pvA1BeUcFahaE&hl=es&ehbc=2E312F";
+			break;
+
+			case "3": 
+				this.mapa = "https://www.google.com/maps/d/embed?mid=1GyQiQX9c7ifJsIpXsZ2WkauEwSI5yR8&ehbc=2E312F";
+			break;
+
+			case "4":
 				this.mapa = 'https://www.google.com/maps/d/u/0/embed?mid=1HMhLdeZ4C6LRKurE_72OAaxtJ5VzMIY&ehbc=2E312F';
-
-				
-
 			break;
 		}
 
